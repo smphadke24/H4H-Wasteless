4ac23acd7ba28d2ac3173291d2bf10343fa3252a
rjquiban <rjquiban@gmail.com>
2021-02-21 09:33:20 -0800

    updated deals

--- h4h/lib/businessPage/deals.dart
+++ h4h/lib/businessPage/deals.dart
@@ -17,15 +17,15 @@ class Deal {
       this.itemOldPrice, this.store, this.expiry);
 
   Deal.fromMap(Map<String, dynamic> map)
-      : itemName = map['name'].toString(),
-        oz = map['ozFoodSaved'],
-        itemDescription = map['sDescription'].toString(),
-        itemPrice = map['newprice'].toString(),
-        itemOldPrice = map['oldprice'].toString(),
-        store = map['store'].toString() + " - " + map['location'].toString(),
-        expiry = map['expiry'].toString(),
-        url = map['url'].toString() ??
-            "https://lh3.googleusercontent.com/proxy/OxgJrFW4vZ3FM-h4xJukhQInsEnrNS5jpNUvlgherKEDf9AU2Aj8LxWRqGLJBTYtcTBPqCqMaWzgX9dVOIQb9gWvfSQG2DW6bYZ-Vmslwa3L1Kl499O29yprBkH9QT3I";
+    : itemName = map['name'].toString(),
+      oz = map['ozFoodSaved'],
+      itemDescription = map['sDescription'].toString(),
+      itemPrice = map['newprice'].toString(),
+      itemOldPrice = map['oldprice'].toString(),
+      store = map['store'].toString() + " - " + map['location'].toString(),
+      expiry = map['expiry'].toString(),
+      url = map['url'].toString() ??
+          "https://lh3.googleusercontent.com/proxy/OxgJrFW4vZ3FM-h4xJukhQInsEnrNS5jpNUvlgherKEDf9AU2Aj8LxWRqGLJBTYtcTBPqCqMaWzgX9dVOIQb9gWvfSQG2DW6bYZ-Vmslwa3L1Kl499O29yprBkH9QT3I";
 }
 
 class Deals extends StatefulWidget {
@@ -63,138 +63,141 @@ class _DealsState extends State<Deals> {
     return Container(
       height: MediaQuery.of(context).size.height * 0.29,
       margin: EdgeInsets.only(left: 22, right: 32, bottom: 22),
-      child: ListView.separated(
-        padding: const EdgeInsets.all(8),
-        itemCount: 3,
-        itemBuilder: (BuildContext context, int index) {
-          return (Container(
-            height: 120,
-            padding: EdgeInsets.all(15.0),
-            decoration: BoxDecoration(
-              color: LightGrey,
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          width: (MediaQuery.of(context).size.width - 30) * 0.6,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Jake's Green Baby Spinach",
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                    fontSize: 17.0,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              SizedBox(
-                                height: 5,
-                              ),
-                            ],
-                          ),
-                        ),
-                        Container(
-                          width: (MediaQuery.of(context).size.width - 30) * 0.2,
-                          height: 20,
-                          decoration: BoxDecoration(
-                            color: DarkGrey,
-                            borderRadius: BorderRadius.circular(100),
-                          ),
-                          child: Center(
-                            child: Text(
-                              "Vegetable",
-                              overflow: TextOverflow.fade,
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 10.0,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Text(
-                      "Farm grown, 100% organic, expires so fuckin quickly.",
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(color: BlueGrey),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
+      padding: const EdgeInsets.all(8),
+      child: ListView(
+        physics: ScrollPhysics(),
+        shrinkWrap: true,
+        children: filtered.map((deal) => _buildTile(context, deal)).toList(),
+      ),
+    );
+  }
+
+  Widget _buildTile(BuildContext context, var deal) {
+    dynamic currentDeal = Deal.fromMap(deal);
+
+    return Container(
+      height: 120,
+      padding: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: LightGrey,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: (MediaQuery.of(context).size.width - 30) * 0.6,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          "\$5.00",
+                          currentDeal.itemName,
+                          overflow: TextOverflow.ellipsis,
                           style: TextStyle(
-                            decoration: TextDecoration.lineThrough,
-                            fontSize: 14.0,
-                          ),
+                            fontSize: 17.0,
+                            fontWeight: FontWeight.bold),
                         ),
-                        Text(
-                          "\$3.50",
-                          style: TextStyle(
-                            color: LimeGreen,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18.0,
-                          ),
+                        SizedBox(
+                          height: 5,
                         ),
                       ],
                     ),
-                    Text(
-                      "Expiration: 03/06/2021",
-                      style: TextStyle(color: LimeGreen),
+                  ),
+                  Container(
+                    width: (MediaQuery.of(context).size.width - 30) * 0.2,
+                    height: 20,
+                    decoration: BoxDecoration(
+                      color: DarkGrey,
+                      borderRadius: BorderRadius.circular(100),
                     ),
-                    InkWell(
-                      onTap: () {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) => PopUp(
-                            url:
-                                "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.commitpoint.com%2Fassets%2Fimages%2Fproducts.png&f=1&nofb=1",
-                            itemName: currentDeal.itemName,
-                            description: currentDeal.itemDescription,
-                            oldPrice: currentDeal.itemOldPrice.toString(),
-                            newPrice: currentDeal.itemPrice.toString(),
-                            expirationDate: currentDeal.expiry,
-                            store: currentDeal.store,
-                            savesWasteOz: currentDeal.oz,
-                          ),
-                        );
-                      },
-                      child: Container(
-                        width: 25.0,
-                        height: 25.0,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: LimeGreen,
-                        ),
-                        child: Icon(
-                          Icons.add_rounded,
+                    child: Center(
+                      child: Text(
+                        currentDeal.itemDescription,
+                        overflow: TextOverflow.fade,
+                        style: TextStyle(
                           color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 10.0,
                         ),
                       ),
-                    )
-                  ],
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(color: BlueGrey),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "\$" + currentDeal.oldItemPrice,
+                    style: TextStyle(
+                      decoration: TextDecoration.lineThrough,
+                      fontSize: 14.0,
+                    ),
+                  ),
+                  Text(
+                    "\$" + currentDeal.itemPrice,
+                    style: TextStyle(
+                      color: LimeGreen,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                "Expiration: " + currentDeal.expiry,
+                style: TextStyle(color: LimeGreen),
+              ),
+              InkWell(
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) => PopUp(
+                      url:
+                          "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.commitpoint.com%2Fassets%2Fimages%2Fproducts.png&f=1&nofb=1",
+                      itemName: currentDeal.itemName,
+                      description: currentDeal.itemDescription,
+                      oldPrice: currentDeal.itemOldPrice.toString(),
+                      newPrice: currentDeal.itemPrice.toString(),
+                      expirationDate: currentDeal.expiry,
+                      store: currentDeal.store,
+                      savesWasteOz: currentDeal.oz,
+                    ),
+                  );
+                },
+                child: Container(
+                  width: 25.0,
+                  height: 25.0,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: LimeGreen,
+                  ),
+                  child: Icon(
+                    Icons.add_rounded,
+                    color: Colors.white,
+                  ),
                 ),
-              ],
-            ),
-          ));
-        },
-        separatorBuilder: (BuildContext context, int index) =>
-            const SizedBox(height: 10),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
